5a7e01b0ec3efa5d4eb7c6c93003125be3fd7f68
KennyR <kennykr@hotmail.co.uk>
2020-06-09 14:39:52 +0100

    Add education form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Blog
+from .models import Blog,Education
 
 
 class BlogForm(forms.ModelForm):
@@ -7,3 +7,9 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('title', 'text',)
+
+
+class EducationForm(forms.ModelForm):
+    class Meta:
+        model = Education
+        fields =('school','start_year','end_year','field_of_study','description',)
