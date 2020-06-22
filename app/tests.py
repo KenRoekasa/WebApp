29b4795285651b92fe70897bf25f72db1d167463
KennyR <kennykr@hotmail.co.uk>
2020-06-22 17:35:44 +0100

    Fixed unit testing with all date formatting and academic projects section

--- app/tests.py
+++ app/tests.py
@@ -4,11 +4,11 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase, Client
 from django.urls import resolve
-from django.utils import timezone
+from django.utils import timezone, dateformat
 
 from .views import home, cv, project_list, blog_list, cv_education_new, cv_education_edit, cv_tech_skills_edit, \
     cv_tech_skills_new, cv_work_exp_edit, cv_work_exp_new, cv_projects_new, cv_projects_edit
-from .models import Blog, Project, Education, TechSkills, WorkExperience,AcademicProjects
+from .models import Blog, Project, Education, TechSkills, WorkExperience, AcademicProjects
 
 
 class HomePageTest(TestCase):
@@ -158,7 +158,6 @@ class CVEducationSectionTest(TestCase):
                                  start_year
                                  =2017, end_year=2021, field_of_study='Comp Sci', id=1)
 
-        print(Education.objects.all()[0].id)
         found = resolve('/cv/edit/education/1/')
         self.assertEqual(found.func, cv_education_edit)
 
@@ -249,7 +248,7 @@ class CVWorkExperienceSectionTest(TestCase):
         response = self.client.get('/cv/edit/workexp/new/')
         self.assertTemplateUsed(response, 'app/cv_work_experience_edit.html')
 
-    def test_work_experience_edit_url_resolves_to_work_experience_editt_view(self):
+    def test_work_experience_edit_url_resolves_to_work_experience_edit_view(self):
         WorkExperience.objects.create(title='CEO', company='Google', description='Working', location='Silicon Valley',
                                       start_date=timezone.now(), end_date=timezone.now(), id=1)
         found = resolve('/cv/edit/workexp/1/')
@@ -259,7 +258,7 @@ class CVWorkExperienceSectionTest(TestCase):
         WorkExperience.objects.create(title='CEO', company='Google', description='Working', location='Silicon Valley',
                                       start_date=timezone.now(), end_date=timezone.now(),
                                       id=1)
-        response = self.client.get('cv/edit/workexp/1/')
+        response = self.client.get('/cv/edit/workexp/1/')
         self.assertTemplateUsed(response, 'app/cv_work_experience_edit.html')
 
     def test_cv_page_has_work_experience_section(self):
@@ -283,34 +282,38 @@ class CVWorkExperienceSectionTest(TestCase):
         self.assertIn('Testing', response.content.decode())
         self.assertIn('UK', response.content.decode())
         self.assertIn('Silicon Valley', response.content.decode())
-        self.assertIn('June 2020-June 2020', response.content.decode())
+        self.assertIn('June 2020 - June 2020', response.content.decode())
 
     def test_cv_new_work_experience_save_POST_request(self):
         response = self.client.post('/cv/edit/workexp/new/',
                                     data={
                                         'title': 'CEO', 'company': 'Google', 'description': 'Working',
                                         'location': 'Silicon Valley',
-                                        'start_date': timezone.now(), 'end_date': timezone.now()})
+                                        'start_date': '2020-06-18', 'end_date': '2020-06-18'})
 
         latest_item = WorkExperience.objects.all()[0]
 
         self.assertEqual(latest_item.title, "CEO")
         self.assertEqual(latest_item.company, "Google")
         self.assertEqual(latest_item.description, "Working")
-        self.assertEqual(latest_item.start_date, timezone.now())
-        self.assertEqual(latest_item.end_date, timezone.now())
+        self.assertEqual(latest_item.start_date_mY(), 'June 2020')
+        self.assertEqual(latest_item.end_date_mY(), 'June 2020')
         self.assertEqual(latest_item.location, "Silicon Valley")
 
     def test_cv_work_experience_edit_save_POST(self):
+        time = timezone.now()
         WorkExperience.objects.create(title='CEO', location='Silicon Valley',
-                                      company='Google', description='Working', start_date='', end_date='')
+                                      company='Google', description='Working', start_date=time,
+                                      end_date=time)
+
         response = self.client.post('/cv/edit/workexp/1/',
                                     data={
                                         'title': 'Owner', 'company': 'Google', 'location': 'Silicon Valley',
                                         'description': 'Working',
-                                        'start_date': timezone.now(), 'end_date': timezone.now()})
+                                        'start_date': dateformat.format(time, 'Y-m-d'),
+                                        'end_date': dateformat.format(time, 'Y-m-d')})
 
-        latest_item = TechSkills.objects.all()[0]
+        latest_item = WorkExperience.objects.all()[0]
 
         self.assertEqual(latest_item.title, "Owner")
 
@@ -333,7 +336,8 @@ class CVAcademicProjectsSectionTest(TestCase):
     def test_cv_academic_projects_edit_page_returns_correct_html(self):
         AcademicProjects.objects.create(title='Flappy Bird', description='Working \n blah blah blah \n this and that',
                                         start_date=timezone.now(), end_date=timezone.now(), id=1)
-        response = self.client.get('cv/edit/projects/1/')
+
+        response = self.client.get('/cv/edit/projects/1/')
         self.assertTemplateUsed(response, 'app/cv_projects_edit.html')
 
     def test_cv_page_has_academic_projects_section(self):
@@ -354,36 +358,35 @@ class CVAcademicProjectsSectionTest(TestCase):
         self.assertIn('Website', response.content.decode())
         self.assertIn('Working', response.content.decode())
         self.assertIn('this assignment \n wooweee wooowee \n those and this', response.content.decode())
-        self.assertIn('June 2020-June 2020', response.content.decode())
+        self.assertIn('June 2020 - June 2020', response.content.decode())
 
     def test_cv_new_academic_projects_save_POST_request(self):
-        response = self.client.post('/cv/edit/workexp/new/',
+        response = self.client.post('/cv/edit/projects/new/',
                                     data={
-                                        'title': 'CEO', 'company': 'Google', 'description': 'Working',
-                                        'location': 'Silicon Valley',
-                                        'start_date': timezone.now(), 'end_date': timezone.now()})
+                                        'title': 'Flappy Bird', 'description': 'Working',
+                                        'start_date': '2020-06-20', 'end_date': '2020-06-20'})
 
-        latest_item = WorkExperience.objects.all()[0]
+        latest_item = AcademicProjects.objects.all()[0]
 
-        self.assertEqual(latest_item.title, "CEO")
-        self.assertEqual(latest_item.company, "Google")
+        self.assertEqual(latest_item.title, "Flappy Bird")
         self.assertEqual(latest_item.description, "Working")
-        self.assertEqual(latest_item.start_date, timezone.now())
-        self.assertEqual(latest_item.end_date, timezone.now())
-        self.assertEqual(latest_item.location, "Silicon Valley")
+        self.assertEqual(latest_item.start_date_mY(), 'June 2020')
+        self.assertEqual(latest_item.end_date_mY(), 'June 2020')
 
     def test_cv_academic_projects_edit_save_POST(self):
-        WorkExperience.objects.create(title='CEO', location='Silicon Valley',
-                                      company='Google', description='Working', start_date='', end_date='')
-        response = self.client.post('/cv/edit/workexp/1/',
+        time = timezone.now()
+        AcademicProjects.objects.create(title='Flappy Bird', description='Working \n blah blah blah \n this and that',
+                                        start_date=time, end_date=time, id=1)
+        response = self.client.post('/cv/edit/projects/1/',
                                     data={
-                                        'title': 'Owner', 'company': 'Google', 'location': 'Silicon Valley',
+                                        'title': 'Website',
                                         'description': 'Working',
-                                        'start_date': timezone.now(), 'end_date': timezone.now()})
+                                        'start_date': dateformat.format(time, 'Y-m-d'),
+                                        'end_date': dateformat.format(time, 'Y-m-d')})
 
-        latest_item = TechSkills.objects.all()[0]
+        latest_item = AcademicProjects.objects.all()[0]
 
-        self.assertEqual(latest_item.title, "Owner")
+        self.assertEqual(latest_item.title, "Website")
 
 
 class CVPageTest(TestCase):
