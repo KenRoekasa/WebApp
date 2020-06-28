1f3050897c7ac6730c8a9f8abebf9b7c6ea1df14
KennyR <kennykr@hotmail.co.uk>
2020-06-28 14:40:21 +0100

    Add register models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Project, Blog
+from .models import Project, Blog, Education, WorkExperience, TechSkills, AcademicProjects
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Blog)
+admin.site.register(Education)
+admin.site.register(TechSkills)
+admin.site.register(AcademicProjects)
+admin.site.register(WorkExperience)
